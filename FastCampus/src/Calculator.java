09879938726d32508cd06c2bcb078b6cd41f2703
SangHui48 <vodlrm123@naver.com>
2024-07-20 13:40:10 +0900

    7/20

---
+++ FastCampus/src/Calculator.java
@@ -0,0 +1,12 @@
+// 클래스 단위로 코딩을 한다.
+// 시작, 메인 클래스
+// JavaSE 프로그램
+public class Calculator {
+    public static void main(String[] args) {
+        int a, b, sum;
+        a = 1;
+        b = 2;
+        sum = a + b;
+        System.out.println(sum);
+    }
+}
