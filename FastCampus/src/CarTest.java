b4d96b776ec67fed7545464f81e2bc05da0f13c3
SangHui48 <vodlrm123@naver.com>
2024-07-20 22:33:48 +0900

    7/20

---
+++ FastCampus/src/CarTest.java
@@ -0,0 +1,12 @@
+public class CarTest {
+    public static void main(String[] args) {
+        String model = "BMW528i";
+        long distance = 1000000L;
+        int price = 9000000;
+        String company = "BMW";
+        char type = 'A';
+        boolean auto = false;
+        int year = 2000;
+        float gasmi = 12.5f;
+    }
+}
