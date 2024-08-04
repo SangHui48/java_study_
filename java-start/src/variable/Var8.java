a7935db0edd14caefd027b1e81b0eecdd23508bc
SangHui48 <vodlrm123@naver.com>
2024-08-04 16:53:39 +0900

    8/4

---
+++ java-start/src/variable/Var8.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127; //-128 ~ 127
+        short s = 32767; // -32768 ~ 32767;
+        int i = 2147483647; // 대충 21억정도
+        long l = 9223372036854775807L; // 뒤에 L붙여야함. 그냥 조오오온나크다
+
+
+        // 실수
+        float f = 10.0f;
+        double d = 10.0;
+
+    }
+}
