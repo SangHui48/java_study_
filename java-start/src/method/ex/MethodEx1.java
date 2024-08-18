b0219c96f0525a46b7d6cdbd510040ae3f636a45
SangHui48 <vodlrm123@naver.com>
2024-08-18 23:11:11 +0900

    8/18 입문완강

---
+++ java-start/src/method/ex/MethodEx1.java
@@ -0,0 +1,13 @@
+package method.ex;
+
+public class MethodEx1 {
+    public static void main(String[] args) {
+        average(1,2,3);
+        average(15,25,35);
+    }
+
+    public static void average(int a, int b, int c) {
+        double avg = (a + b + c) / 3.0;
+        System.out.println("평균값: " + avg);
+    }
+}
---
+++ java-start/src/method/ex/MethodEx2.java
@@ -0,0 +1,16 @@
+package method.ex;
+
+public class MethodEx2 {
+    public static void main(String[] args) {
+        String message = "Hello World!";
+        printMessage(message, 3);
+        printMessage(message, 5);
+        printMessage(message, 7);
+    }
+
+    public static void printMessage(String message, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(message);
+        }
+    }
+}
