88fb8f3161ecf11e8e345654ac88f82118bc9a6b
SangHui48 <vodlrm123@naver.com>
2024-08-20 20:18:04 +0900

    8/20 null

---
+++ java-basic/src/ref/InitMain.java
@@ -0,0 +1,12 @@
+package ref;
+
+public class InitMain {
+    public static void main(String[] args) {
+        InitData data = new InitData();
+        System.out.println(data.value1);
+        System.out.println(data.value2);
+
+//        int a;
+//        System.out.println("a = " + a);
+    }
+}
